d12ccbe49fe0e3cc31b78a780f2777a7eb315b44
Yash <yash.parmar@einfochips.com>
2024-07-30 19:10:18 +0530

    files is added

---
+++ Day3/fullstack-docker-app/backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+    user: 'user',
+    host: 'postgres-container',
+    database: 'mydatabase',
+    password: 'password',
+    port: 5432,
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node.js and Docker!');
+});
+
+app.get('/data', async (req, res) => {
+    const client = await pool.connect();
+    const result = await client.query('SELECT NOW()');
+    client.release();
+    res.send(result.rows);
+});
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+});
